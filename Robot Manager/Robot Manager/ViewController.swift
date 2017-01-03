79763b19257016057170929bb42f1fe81def69a2
Kurt Bowen <kurtisbowen992@Kurts-Mac.local>
2017-01-02 22:05:47 -0600

    Logo & useable Keyboard

--- Robot Manager/Robot Manager/ViewController.swift
+++ Robot Manager/Robot Manager/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK: properties
+    @IBOutlet weak var teamNameField: UITextField!
+    @IBOutlet weak var teamNameLabel: UILabel!
+    @IBOutlet weak var teamStrength: UISlider!
+    @IBOutlet weak var teamGameMode: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks.
+        teamNameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: actions
+    @IBAction func teamSubmit(_ sender: UIButton) {
+        teamNameLabel.text = teamNameField.text! + "|" + String(format:"%.2f", (teamStrength.value * 100)) + "|" + teamGameMode.selectedSegmentIndex.description;
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField){
+        teamNameLabel.text = teamNameField.text
+    }
 }
 
