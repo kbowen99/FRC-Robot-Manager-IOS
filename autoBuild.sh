b33ab62dd8771463d781cf3a29a7f64c54ed1be4
Kurt Bowen <kurtisbowen99@gmail.com>
2017-02-09 15:00:38 -0600

    Works

--- autoBuild.sh
+++ autoBuild.sh
@@ -1,9 +1,9 @@
 rm RobotManager.ipa
 cd Robot\ Manager/
 xcodebuild
-mv build/Release-iphoneos/ build/payload/
+mv build/Release-iphoneos/ build/Payload/
 cd build/
-zip -r RobotManager.ipa payload/
+zip -r RobotManager.ipa Payload/
 cd ..
 mv build/RobotManager.ipa RobotManager.ipa
 rm -d -f -R build/
